2eb74fbacaab0ba4ff033113a0e3234cb1161ce2
Papillon6814 <so1rou.k@gmail.com>
2022-11-30 21:31:25 +0900

    feat: timer

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,7 +1,10 @@
 mod utils;
 
+extern crate web_sys;
+
 use wasm_bindgen::prelude::*;
 use std::fmt;
+use web_sys::console;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -22,6 +25,23 @@ pub struct Universe {
 	width: u32,
 	height: u32,
 	cells: Vec<Cell>,
+}
+
+pub struct Timer<'a> {
+	name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+	pub fn new(name: &'a str) -> Timer<'a> {
+		console::time_with_label(name);
+		Timer { name }
+	}
+}
+
+impl<'a> Drop for Timer<'a> {
+	fn drop(&mut self) {
+		console::time_end_with_label(self.name);
+	}
 }
 
 impl Universe {
@@ -113,6 +133,7 @@ impl Universe {
 	}
 
 	pub fn tick(&mut self) {
+		let _timer = Timer::new("Universe::tick");
 		let mut next = self.cells.clone();
 
 		for row in 0..self.height {
